f020b1cb8168736bff327faf0cfa475cb9dd0777
GRUPO FUMINSUMOS SAC <GRUPO FUMINSUMOS SAC@servidor2>
2018-12-08 11:31:50 -0500

    BD

---
+++ web/bd/procedimiento.sql
@@ -0,0 +1,35 @@
+/*
+SQLyog Ultimate v12.09 (64 bit)
+MySQL - 10.1.32-MariaDB : Database - bdhotelfinal
+*********************************************************************
+*/
+
+/*!40101 SET NAMES utf8 */;
+
+/*!40101 SET SQL_MODE=''*/;
+
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+CREATE DATABASE /*!32312 IF NOT EXISTS*/`bdhotelfinal` /*!40100 DEFAULT CHARACTER SET latin1 */;
+
+USE `bdhotelfinal`;
+
+/* Procedure structure for procedure `sp_DevuelveFila` */
+
+/*!50003 DROP PROCEDURE IF EXISTS  `sp_DevuelveFila` */;
+
+DELIMITER $$
+
+/*!50003 CREATE DEFINER=`root`@`localhost` PROCEDURE `sp_DevuelveFila`(tipo varchar(50))
+BEGIN
+    SELECT nro FROM (SELECT @row := @row + 1 AS nro, t.*
+FROM tipo_doccliente t, (SELECT @row := 0) r)AS tablaTemporal WHERE documento = tipo;
+    END */$$
+DELIMITER ;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
